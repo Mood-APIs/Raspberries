3d8340ee12d079d0e6fc088a67ad0baab8afe991
Dimitris Komninos <44092571+LightingSpider@users.noreply.github.com>
2021-11-18 16:09:09 +0200

    Update monitor-app.sh

--- monitor-app.sh
+++ monitor-app.sh
@@ -6,15 +6,30 @@ RECORD_PID=$(echo $ALL_RECORDS_PIDS | awk '{print $2}')
 FFMPEG_PID=$(ps aux | grep ffmpeg | grep -v grep | awk '{print $2}')
 
 USER=$(whoami)
+
+# Kill record processes
+if [ -z "$RECORD_PID" ]
+then
+	echo "no record processes"
+else
+	kill $RECORD_PID &&
+	sleep 2
+fi
+
+# Kill ffmpeg processes
+if [ -z "$FFMPEG_PID" ]
+then
+	echo "no ffmpeg processes"
+else
+	kill $FFMPEG_PID &&
+	sleep 2
+fi
+
 if [ -z "$APP_PID" ]
 then
 	/home/$USER/mood-scripts/run.sh
 else
 	kill $APP_PID &&
-	sleep 2 &&
-	kill $RECORD_PID &&
-	sleep 2 &&
-	kill $FFMPEG_PID &&
 	sleep 2 &&
 	/home/$USER/mood-scripts/run.sh
 fi
