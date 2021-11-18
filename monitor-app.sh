757d5be3ad64a05f0470d028f95b830bf4d8a72f
Dimitris Komninos <44092571+LightingSpider@users.noreply.github.com>
2021-11-18 13:41:31 +0200

    Kill ffmpeg processes also.

--- monitor-app.sh
+++ monitor-app.sh
@@ -1,6 +1,9 @@
 APP_PID=$(ps aux | grep app.py | grep -v grep | awk '{print $2}')
+
 ALL_RECORDS_PIDS=$(ps aux | agrep record | grep -v grep | awk '{print $2}')
 RECORD_PID=$(echo $ALL_RECORDS_PIDS | awk '{print $2}')
+
+FFMPEG_PID=$(ps aux | grep ffmpeg | grep -v grep | awk '{print $2}')
 
 USER=$(whoami)
 if [ -z "$APP_PID" ]
@@ -10,6 +13,8 @@ else
 	kill $APP_PID &&
 	sleep 2 &&
 	kill $RECORD_PID &&
+	sleep 2 &&
+	kill $FFMPEG_PID &&
 	sleep 2 &&
 	/home/$USER/mood-scripts/run.sh
 fi
