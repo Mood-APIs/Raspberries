49c5185f9ed9750df11ef3fc56cc8e9f0331d47c
Dimitris Komninos <44092571+LightingSpider@users.noreply.github.com>
2021-11-18 13:35:50 +0200

    Kill all record processes.

--- monitor-app.sh
+++ monitor-app.sh
@@ -1,10 +1,15 @@
-APP_PID=$(ps aux | grep app.py | grep -v grep | awk '{print $2}') &&
+APP_PID=$(ps aux | grep app.py | grep -v grep | awk '{print $2}')
+ALL_RECORDS_PIDS=$(ps aux | agrep record | grep -v grep | awk '{print $2}')
+RECORD_PID=$(echo $ALL_RECORDS_PIDS | awk '{print $2}')
+
 USER=$(whoami)
 if [ -z "$APP_PID" ]
 then
 	/home/$USER/mood-scripts/run.sh
 else
 	kill $APP_PID &&
+	sleep 2 &&
+	kill $RECORD_PID &&
 	sleep 2 &&
 	/home/$USER/mood-scripts/run.sh
 fi
