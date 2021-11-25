3ea1d4e31d1e8d56c88c3b37de12bdee14d068a5
airth <jimkomninos@hotmail.com>
2021-11-26 01:18:09 +0200

    Initial Commit.

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ import requests
 from datetime import datetime
 import subprocess
 import shlex
-
+import sched
+import time
 import os
 
-bar_id = '84:d8:1b:8c:b3:fb'
-path_file = f'/home/{os.getlogin()}/final_song.wav'
-log_path = f'/home/{os.getlogin()}/logs/out-' + datetime.now().strftime('%d-%m-%y') + '.log'
+bar_id = 'E2:63:DA:BA:56:81'
+path_file = f'/home/{os.getlogin()}/final_song.mp3'
 
 # identity: rasp_pi
 access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzNTQ1Mjg3MywianRpIjoiMjQ3NmJlOTktNDQ2Yi00ZTBjLTk2NmQtNjBkOTBjODgzMWE4IiwibmJmIjoxNjM1NDUyODczLCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoicmFzcF9waSIsImV4cCI6MTYzNTQ3NDQ3M30.U5i9eIlqziPgPGKJviLEvgBuloNIXR9iPWsPRIywaao'
 ref_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzNTQ1Mjg3MywianRpIjoiMmEwMTMyNzUtOGFmYy00NmQyLWIwNzctZTZiZjEwMGJkNzE4IiwibmJmIjoxNjM1NDUyODczLCJ0eXBlIjoicmVmcmVzaCIsInN1YiI6InJhc3BfcGkiLCJleHAiOjE3MzAwNjA4NzN9.f7JbSX_aCu87DNg2gzdrpAZJvABbrgLyea9XSjatVlc'
 
-def init():
+global start_time, end_time, record_time
+
+def update_init_settings():
 
     url = "https://r9rjketed6.execute-api.eu-south-1.amazonaws.com/dev/init"
     payload = {}
@@ -23,7 +25,13 @@ def init():
     response = requests.request("GET", url, headers=headers, data=payload, files=files)
     print(response.text)
 
-    return response.json()
+    global start_time, end_time, record_time
+
+    settings = response.json()['settings']
+    basic_timeframe = settings['basic_timeframe']
+    start_time = datetime.strptime(basic_timeframe['start'], '%Y-%m-%d %H:%M:%S')
+    end_time = datetime.strptime(basic_timeframe['end'], '%Y-%m-%d %H:%M:%S')
+    record_time = settings['record_timeOffset']
 
 def new_song(token):
 
@@ -80,17 +88,13 @@ def upload():
     print('Stop uploading -- ' + datetime_str)
 
 
-print(f'Running at ({os.getpid()})')
-init_response = init()
-if init_response['statusCode'] == 200:
-    settings = init_response['settings']
+while True:
 
-    # Read recording timeframes
-    basic_timeframe = settings['basic_timeframe']
-    start_time = datetime.strptime(basic_timeframe['start'], '%Y-%m-%d %H:%M:%S')
-    end_time = datetime.strptime(basic_timeframe['end'], '%Y-%m-%d %H:%M:%S')
+    print(f'Running at ({os.getpid()})')
+
+    update_init_settings()
     now_time = datetime.now()
-    
+
     # Out of recording timeframe
     if now_time > end_time or now_time < start_time:
         print('Out of recording timeframe!')
@@ -98,11 +102,15 @@ if init_response['statusCode'] == 200:
         print(f'Start time: {str(start_time)}')
         print(f'End time: {str(end_time)}')
         print('----------------------------------------------------------------')
+        time.sleep(1800)
 
     # Recording
     while start_time < now_time < end_time:
-        record_time = settings['record_timeOffset']
         subprocess.call(shlex.split(f'/home/{os.getlogin()}/mood-scripts/record.sh ' + str(record_time)))
         upload()
         print('----------------------------------------------------------------')
 
+        # Check if a half-hour passed
+        minutes = datetime.now().minute
+        if minutes % 30 <= 3:
+            update_init_settings()
