f71f771481726c1b3cd9a7c96006b964da74f5ce
airth <jimkomninos@hotmail.com>
2021-11-27 17:31:00 +0200

    Update the log file

--- app.py
+++ app.py
@@ -21,7 +21,10 @@ def update_init_settings():
     # global log_file
     date_now = datetime.now().strftime('%d-%m-%y')
     log_file = f'/home/{os.getlogin()}/logs/out-{date_now}.log'
-    sys.stdout = open(log_file, 'a')
+    if os.path.isfile(log_file):
+        sys.stdout = open(log_file, 'a')
+    else:
+        sys.stdout = open(log_file, 'w')
 
     url = "https://r9rjketed6.execute-api.eu-south-1.amazonaws.com/dev/init"
     payload = {}
