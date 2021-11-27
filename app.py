068f582110af976dae5d5e75495ef4b9cbc5e8b3
airth <jimkomninos@hotmail.com>
2021-11-27 14:32:48 +0200

    Update the log file

--- app.py
+++ app.py
@@ -21,79 +21,87 @@ def update_init_settings():
     global log_file
     date_now = datetime.now().strftime('%d-%m-%Y')
     log_file = f'/home/{os.getlogin()}/logs/out-{date_now}.log'
-    sys.stdout = open(log_file, 'wt')
+    with open(log_file, 'a') as f:
+        sys.stdout = f
 
-    url = "https://r9rjketed6.execute-api.eu-south-1.amazonaws.com/dev/init"
-    payload = {}
-    files = {}
-    headers = {}
+        url = "https://r9rjketed6.execute-api.eu-south-1.amazonaws.com/dev/init"
+        payload = {}
+        files = {}
+        headers = {}
 
-    response = requests.request("GET", url, headers=headers, data=payload, files=files)
-    print(response.text)
+        response = requests.request("GET", url, headers=headers, data=payload, files=files)
+        print(response.text)
 
-    global start_time, end_time, record_time
+        global start_time, end_time, record_time
 
-    settings = response.json()['settings']
-    basic_timeframe = settings['basic_timeframe']
-    start_time = datetime.strptime(basic_timeframe['start'], '%Y-%m-%d %H:%M:%S')
-    end_time = datetime.strptime(basic_timeframe['end'], '%Y-%m-%d %H:%M:%S')
-    record_time = settings['record_timeOffset']
-
-
+        settings = response.json()['settings']
+        basic_timeframe = settings['basic_timeframe']
+        start_time = datetime.strptime(basic_timeframe['start'], '%Y-%m-%d %H:%M:%S')
+        end_time = datetime.strptime(basic_timeframe['end'], '%Y-%m-%d %H:%M:%S')
+        record_time = settings['record_timeOffset']
 
 def new_song(token):
 
-    url = "https://r9rjketed6.execute-api.eu-south-1.amazonaws.com/dev/new_song/" + bar_id
-    payload = {}
-    files = [
-      ('file', ('2021-03-18-10_47_59.mp3', open(path_file, 'rb'), 'audio/mpeg'))
-    ]
-    headers = {
-      'Authorization': 'Bearer ' + token
-    }
+    with open(log_file, 'a') as f:
+        sys.stdout = f
 
-    response = requests.request("POST", url, headers=headers, data=payload, files=files)
-    print(response.text)
+        url = "https://r9rjketed6.execute-api.eu-south-1.amazonaws.com/dev/new_song/" + bar_id
+        payload = {}
+        files = [
+          ('file', ('2021-03-18-10_47_59.mp3', open(path_file, 'rb'), 'audio/mpeg'))
+        ]
+        headers = {
+          'Authorization': 'Bearer ' + token
+        }
 
-    return response.json()
+        response = requests.request("POST", url, headers=headers, data=payload, files=files)
+        print(response.text)
+
+        return response.json()
 
 def refresh_token(token):
 
-    url = "https://r9rjketed6.execute-api.eu-south-1.amazonaws.com/dev/refresh_token"
-    payload = {}
-    files = {}
-    headers = {
-        'Authorization': 'Bearer ' + token
-    }
+    with open(log_file, 'a') as f:
+        sys.stdout = f
 
-    response = requests.request("PUT", url, headers=headers, data=payload, files=files)
-    print(response.text)
+        url = "https://r9rjketed6.execute-api.eu-south-1.amazonaws.com/dev/refresh_token"
+        payload = {}
+        files = {}
+        headers = {
+            'Authorization': 'Bearer ' + token
+        }
 
-    return response.json()['access_token']
+        response = requests.request("PUT", url, headers=headers, data=payload, files=files)
+        print(response.text)
+
+        return response.json()['access_token']
 
 def upload():
 
-    now = datetime.now()
-    datetime_str = now.strftime("%Y-%m-%d %H:%M:%S")
-    print('Start uploading -- ' + datetime_str + '\n')
+    with open(log_file, 'a') as f:
+        sys.stdout = f
 
-    global access_token
-    response = new_song(access_token)
+        now = datetime.now()
+        datetime_str = now.strftime("%Y-%m-%d %H:%M:%S")
+        print('Start uploading -- ' + datetime_str + '\n')
 
-    # Check if access_token is expired
-    if 'error' in response.keys() and response['error']['status'] == '402':
+        global access_token
+        response = new_song(access_token)
 
-        # Refresh it & try again
-        access_token = refresh_token(ref_token)
-        new_song(access_token)
+        # Check if access_token is expired
+        if 'error' in response.keys() and response['error']['status'] == '402':
 
-    # Spotify token expired
-    if 'statusCode' in response.keys() and response['statusCode'] == 408:
-        new_song(access_token)
+            # Refresh it & try again
+            access_token = refresh_token(ref_token)
+            new_song(access_token)
 
-    now = datetime.now()
-    datetime_str = now.strftime("%Y-%m-%d %H:%M:%S")
-    print('Stop uploading -- ' + datetime_str)
+        # Spotify token expired
+        if 'statusCode' in response.keys() and response['statusCode'] == 408:
+            new_song(access_token)
+
+        now = datetime.now()
+        datetime_str = now.strftime("%Y-%m-%d %H:%M:%S")
+        print('Stop uploading -- ' + datetime_str)
 
 
 while True:
@@ -101,32 +109,35 @@ while True:
     # Set the correct path for th log file
     update_init_settings()
 
-    print(f'Running at ({os.getpid()})')
+    with open(log_file, 'a') as f:
+        sys.stdout = f
 
-    # Out of recording timeframe
-    now_time = datetime.now()
-    if now_time > end_time or now_time < start_time:
-        print('Out of recording timeframe!')
-        print(f'Now time: {str(now_time)}')
-        print(f'Start time: {str(start_time)}')
-        print(f'End time: {str(end_time)}')
-        print('----------------------------------------------------------------')
-        time.sleep(1800)
+        print(f'Running at ({os.getpid()})')
 
-    # Recording
-    while start_time < now_time < end_time:
+        # Out of recording timeframe
+        now_time = datetime.now()
+        if now_time > end_time or now_time < start_time:
+            print('Out of recording timeframe!')
+            print(f'Now time: {str(now_time)}')
+            print(f'Start time: {str(start_time)}')
+            print(f'End time: {str(end_time)}')
+            print('----------------------------------------------------------------')
+            time.sleep(1800)
 
-        print('-----------------------------')
-        print(f'Now time: {str(now_time)}')
-        print(f'Start time: {str(start_time)}')
-        print(f'End time: {str(end_time)}')
-        print('-----------------------------')
+        # Recording
+        while start_time < now_time < end_time:
 
-        subprocess.call(shlex.split(f'/home/{os.getlogin()}/mood-scripts/record.sh ' + str(record_time)))
-        upload()
-        print('----------------------------------------------------------------')
+            print('-----------------------------')
+            print(f'Now time: {str(now_time)}')
+            print(f'Start time: {str(start_time)}')
+            print(f'End time: {str(end_time)}')
+            print('-----------------------------')
 
-        # Check if a half-hour passed
-        minutes = datetime.now().minute
-        if minutes % 30 <= 3:
-            update_init_settings()
+            subprocess.call(shlex.split(f'/home/{os.getlogin()}/mood-scripts/record.sh ' + str(record_time)))
+            upload()
+            print('----------------------------------------------------------------')
+
+            # Check if a half-hour passed
+            minutes = datetime.now().minute
+            if minutes % 30 <= 3:
+                update_init_settings()
