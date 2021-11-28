fd9827439f6e6d6da9425ba6003fd6a250f847c2
airth <jimkomninos@hotmail.com>
2021-11-28 13:54:10 +0200

    remove "sudo" from record.sh

--- record.sh
+++ record.sh
@@ -8,7 +8,7 @@ FINAL_SONG_FILE=/home/$USER/final_song.mp3
 
 # shellcheck disable=SC2046
 echo 'Start recording   -- '$(date '+%d/%m/%Y %H:%M:%S') &&
-sudo arecord -t wav -B $(expr $1 \* 1000 \* 60) -f S16_LE -c 2 -r 44100 -d $1 --device="hw:1,0" "$TEMP_SONG_FILE" &&
+arecord -t wav -B $(expr $1 \* 1000 \* 60) -f S16_LE -c 2 -r 44100 -d $1 --device="hw:1,0" "$TEMP_SONG_FILE" &&
 # sudo arecord -f S16_LE -r 11025 -d $1 --device="hw:1,0" ~/temp_song.wav &&
 echo 'Stop recording    -- '$(date '+%d/%m/%Y %H:%M:%S') &&
 echo 'Convert to mp3    -- '$(date '+%d/%m/%Y %H:%M:%S')  &&
